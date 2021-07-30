4665aba94bd1bbde77c4d8b0792d2871115aed4b
Jorge Canario <jorgeic.767@gmail.com>
2021-07-30 17:11:36 -0400

    fix bug in footertest

--- src/test/java/com/WebPage/FeatureTest/FooterTest.java
+++ src/test/java/com/WebPage/FeatureTest/FooterTest.java
@@ -6,7 +6,6 @@ import org.testng.Reporter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -15,28 +14,37 @@ import org.openqa.selenium.WebElement;
 
 
 public class FooterTest extends FeatureTest{
-	By footerInformationLotation = By.id("block_contact_infos"); 
-	Iterator<String> expectedFooterInformations = new ArrayList<>(Arrays.asList("Selenium Framework, Research Triangle Park, North Carolina, USA", 
-              "better_call_saul_appearance", "Call us now: (347) 466-7432",
-              "Email: support@seleniumframework.com" )).iterator();
+	protected By footerInformationLotation = By.id("block_contact_infos"); 
+	protected ArrayList<String> expectedFooterInformations = new ArrayList<>(Arrays.asList("Store information\nSelenium Framework, Research Triangle Park, North Carolina, USA", 
+              "Call us now: (347) 466-7432",
+              "Email: support@seleniumframework.com" ));
 	
   @Test
   public void validateFooterInformation() {
-	  Boolean result=false;
+	  Boolean result=true;
 	  jsExecutor.executeScript( "window.scrollTo(0, document.body.scrollHeight)");
 	  
 	  try {
 		  WebElement footerInformation = driver.findElement(footerInformationLotation);
 		  String footerInformationText = footerInformation.getText();
-		  while(expectedFooterInformations.hasNext()) {
-			  result = footerInformationText.contains(expectedFooterInformations.next()); 
+		  Reporter.log("[INFORMATION ON FOOTER:] "+ footerInformationText+"\n\n");
+		  
+		  for(String expectedValue: expectedFooterInformations ) {
+			  Reporter.log("[EXPECTED INFORMATION VALUES:] "+ expectedValue+"\n\n");
+			  if(result)
+				  result = footerInformationText.contains(expectedValue); 
+			  else
+				  break;
+			  Reporter.log("[COMPARISON RESULT:] "+ result+"\n\n");
 		  }
 		} 
 	  catch(NoSuchElementException e) {
-			Reporter.log("Page has no footer information");
+			Reporter.log("[ERROR:] Page has no footer information");
+			result=false;
 		}
 	  catch (TimeoutException e) {
-		  	Reporter.log("Page took too long to respond");
+		  	Reporter.log("[ERROR:] Page took too long to respond");
+		  	result=false;
 		}
 	  
 	  AssertJUnit.assertTrue("Footer information is not matching expected Information", result);	  
